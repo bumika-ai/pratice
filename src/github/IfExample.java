f55db425711c91c28cdeb3668d2d097b5bb5c811
swathibumika <swathibumika@LAPTOP-3MM9SB8U>
2021-06-24 21:50:26 +0530

    first

---
+++ src/github/IfExample.java
@@ -0,0 +1,10 @@
+package github;
+
+public class IfExample {  
+public static void main(String[] args) {  
+    int num=20;  
+    if(num>0){  
+        System.out.print("Positive");  
+    }  
+}  
+}
